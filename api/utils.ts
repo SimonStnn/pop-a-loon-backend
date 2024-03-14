fabe8586f7585b14e720f87ba7706e4b07566255
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-14 18:03:54 +0100

    Add validateEnv function to check for required environment variables

---
+++ api/utils.ts
@@ -0,0 +1,11 @@
+export const validateEnv = () => {
+  if (!process.env.DATABASE_URL) {
+    console.error('No database URL provided');
+    process.exit(1);
+  }
+
+  if (!process.env.JWT_SECRET) {
+    console.error('No secret key provided');
+    process.exit(1);
+  }
+};
