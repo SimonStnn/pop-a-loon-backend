90d439cb97594896b8dad0022fefe92e084271fa
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-14 18:43:56 +0100

    Revert "Add JWT token verification and generation functions"
    This reverts commit 2730bd7650d07b26910bac5ac15c9cd091769009.

--- api/utils.ts
+++ api/utils.ts
@@ -1,5 +1,3 @@
-import jwt from 'jsonwebtoken';
-
 export const validateEnv = () => {
   if (!process.env.DATABASE_URL) {
     console.error('No database URL provided');
@@ -15,23 +13,4 @@ export const validateEnv = () => {
     console.error('No token expiration provided');
     process.exit(1);
   }
-};
-
-export const verifyToken = (token: string) => {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(decoded);
-    });
-  });
-};
-
-export const generateToken = (
-  payload: any,
-  secret: string,
-  expiresIn: string
-) => {
-  return jwt.sign(payload, secret, { expiresIn });
 };
