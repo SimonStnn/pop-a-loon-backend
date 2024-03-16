63f8385b2f6ac5da7b6be5666d94e369cb2d62e6
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-16 11:43:38 +0100

    Add MongooseDocumentType to const.ts

--- api/const.ts
+++ api/const.ts
@@ -2,6 +2,13 @@ export type JWTSignature = {
   id: string;
 };
 
+import mongoose from 'mongoose';
+
+export type MongooseDocumentType<T> = mongoose.Document<unknown, {}, T> &
+  T & {
+    _id: mongoose.Types.ObjectId;
+  };
+
 declare module 'express' {
   export interface Request {
     jwt?: JWTSignature | undefined;
