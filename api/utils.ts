255aae411c9bb197eea5ca3707577813ec2c7844
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-16 11:54:52 +0100

    Add formatUser function to utils.ts

--- api/utils.ts
+++ api/utils.ts
@@ -1,3 +1,6 @@
+import { type UserDocumentType } from './schemas/user';
+import { type CountDocumentType } from './schemas/count';
+
 export const validateEnv = () => {
   if (!process.env.DATABASE_URL) {
     console.error('No database URL provided');
@@ -13,4 +16,18 @@ export const validateEnv = () => {
     console.error('No token expiration provided');
     process.exit(1);
   }
+};
+
+export const formatUser = (
+  user: UserDocumentType,
+  count: CountDocumentType,
+) => {
+  return {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    count: count.count,
+    updatedAt: user.updatedAt,
+    createdAt: user._id.getTimestamp(),
+  };
 };
