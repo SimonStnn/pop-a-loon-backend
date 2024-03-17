14aa53815fbfe389dd21bdd276f3cec535cfb683
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-17 08:49:01 +0100

    Update swagger options to include YAML file

--- api/swagger.ts
+++ api/swagger.ts
@@ -8,40 +8,5 @@ export const swaggerOptions: swaggerJsdoc.Options = {
       version: '0.1.0',
     },
   },
-  apis: ['./api/**/*.ts'],
+  apis: ['./api/swagger.yaml', './api/**/*.ts'],
 };
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Error:
- *       properties:
- *         error:
- *           type: string
- *           description: The error message
- *           required: true
- *     User:
- *       properties:
- *         id:
- *           type: string
- *           description: The user's id
- *         username:
- *           type: string
- *           description: The user's username
- *         count:
- *           type: int
- *           description: The user's count
- *         updatedAt:
- *           type: string
- *           format: date-time
- *           description: The date the user was last updated
- *         createdAt:
- *           type: string
- *           format: date-time
- *           description: The date the user was created
- *       required:
- *         - id
- *         - username
- *         - updatedAt
- */
