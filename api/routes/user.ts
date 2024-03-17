d1f0508db19eb4a72bc1186b5b60ce5c378cade8
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-17 08:49:52 +0100

    Remove duplicate swaggerUi documentation

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -6,34 +6,8 @@ import Count from '../schemas/count';
 import { JWTSignature } from '../const';
 import { formatUser } from '../utils';
 
-/**
- * @swagger
- * tags:
- *   name: User
- *   description: User management
- */
-
 const router = express.Router();
 
-/**
- * @swagger
- * /api/user/{id}:
- *   get:
- *     summary: Get a user by id
- *     tags: [User]
- *     responses:
- *       200:
- *         description: The created user
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       404:
- *         description: Not found
- *       500:
- *         description: Internal server error
- *
- */
 router.get('/:id', async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
@@ -76,38 +50,6 @@ router.get('/:id/count', async (req: Request, res: Response) => {
   }
 });
 
-/**
- * @swagger
- * /api/user/{id}/count/increment:
- *   post:
- *     summary: Increment the count of a user
- *     tags: [User]
- *     responses:
- *       200:
- *         description:
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: string
- *                   description: The user's id
- *                 count:
- *                   type: int
- *                   description: The user's count
- *                 updatedAt:
- *                   type: string
- *                   format: date-time
- *                   description: The date the user was last updated
- *       403:
- *         description: Forbidden
- *       404:
- *         description: Not found
- *       500:
- *         description: Internal server error
- *
- */
 router.post('/:id/count/increment', async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
@@ -139,51 +81,6 @@ router.post('/:id/count/increment', async (req: Request, res: Response) => {
   }
 });
 
-/**
- * @swagger
- * /api/user/new:
- *   post:
- *     summary: Create a new user
- *     tags: [User]
- *     parameters:
- *      - in: query
- *        name: username
- *        required: true
- *        schema:
- *          type: string
- *          description: The username of the user
- *      - in: query
- *        name: email
- *        required: true
- *        schema:
- *          type: string
- *          description: The email of the user
- *      - in: query
- *        name: password
- *        required: true
- *        schema:
- *          type: string
- *          description: The password of the user
- *     responses:
- *       200:
- *         description: The created user
- *         content:
- *           application/json:
- *             schema:
- *               allOf:
- *                 - type: object
- *                   properties:
- *                     token:
- *                       type: string
- *                       description: The user's token
- *                       required: false
- *                 - $ref: '#/components/schemas/User'
- *       400:
- *         description: User already exists
- *       500:
- *         description: Internal server error
- *
- */
 router.post('/new', async (req: Request, res: Response) => {
   try {
     const { username, email, password, initialCount } = req.query;
@@ -225,46 +122,6 @@ router.post('/new', async (req: Request, res: Response) => {
   }
 });
 
-/**
- * @swagger
- * /api/user/{id}:
- *   put:
- *     summary: Update a user by id
- *     tags: [User]
- *     parameters:
- *       - in: query
- *         name: username
- *         required: false
- *         schema:
- *           type: string
- *           description: The username of the user
- *       - in: query
- *         name: email
- *         required: false
- *         schema:
- *           type: string
- *           description: The email of the user
- *       - in: query
- *         name: password
- *         required: false
- *         schema:
- *           type: string
- *           description: The password of the user
- *     responses:
- *       200:
- *         description: The created user
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       403:
- *         description: Forbidden
- *       404:
- *         description: Not found
- *       500:
- *         description: Internal server error
- *
- */
 router.put('/:id', async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
