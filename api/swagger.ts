944fcce8a5059b0353e5723e30a94c577fdf71a7
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-19 15:57:44 +0100

    Add development server URL to Swagger options

--- api/swagger.ts
+++ api/swagger.ts
@@ -7,6 +7,12 @@ export const swaggerOptions: swaggerJsdoc.Options = {
       title: 'Pop-a-loon API',
       version: process.env.npm_package_version || '0.0.0',
     },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
   },
   apis: ['./api/swagger.yaml', './api/**/*.ts'],
 };
