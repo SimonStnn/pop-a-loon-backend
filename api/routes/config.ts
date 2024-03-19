66b4733475bc1b39ca4b89f5e6a29f6d5bc93da6
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-19 19:32:34 +0100

    Add badge configuration to API response

--- api/routes/config.ts
+++ api/routes/config.ts
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
   res.json({
+    badge: {
+      color: '#26282b',
+      backgroundColor: '#7aa5eb',
+    },
     spawnInterval: {
       min: 1000,
       max: 10 * 60000,
