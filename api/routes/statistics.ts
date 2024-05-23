009511958ef58b18603ad11b68daab88d5bd3150
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-23 09:41:43 +0200

    Update import statement in statistics route

--- api/routes/statistics.ts
+++ api/routes/statistics.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { fetchTotalPopped } from '@/utils';
+import { fetchTotalPopped } from '../utils';
 
 const router = express.Router();
 
