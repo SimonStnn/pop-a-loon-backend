492ac4c63f35358b71faef425243c81035cceca0
SimonStnn <simon.stijnen.23@gmail.com>
2024-06-20 23:47:33 +0200

    Give counthistory the correct name

--- api/schemas/counthistory.ts
+++ api/schemas/counthistory.ts
@@ -1,20 +1,14 @@
 import mongoose from 'mongoose';
 import { balloonTranslation, MongooseDocumentType } from '../const';
 
-export const name = 'Count';
+export const name = 'CountHistory';
 
 export type CountHistory = {
-  _id: mongoose.Schema.Types.ObjectId;
   user: mongoose.Schema.Types.ObjectId;
   type: number;
 };
 
 export const schema = new mongoose.Schema<CountHistory>({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
