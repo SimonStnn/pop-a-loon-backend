dd63099433ef3418cdbe47ff3ff2948073fcd33a
SimonStnn <simon.stijnen.23@gmail.com>
2024-07-09 19:53:12 +0200

    Validate leaderboard inputs #29

--- api/routes/leaderboard.ts
+++ api/routes/leaderboard.ts
@@ -7,46 +7,47 @@ import {
   formatUser,
   getUserCount,
 } from '../utils';
+import { matchedData, query, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/', async (req: Request, res: Response) => {
-  const limit = parseInt(req.query.limit as string) || 10;
-  const skip = parseInt(req.query.skip as string) || 0;
+router.get(
+  '/',
+  query('limit').isInt({ min: 1, max: 10 }).optional(),
+  query('skip').isInt({ min: 0 }).optional(),
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: 'Invalid query parameters' });
+    }
+    const data = matchedData(req) as { limit?: number; skip?: number };
+    const limit = data.limit || 10;
+    const skip = data.skip || 0;
+    const userId = req.jwt!.id;
+    const user = await User.findById(userId).exec();
+    const userCount = await getUserCount(userId, res);
 
-  if (limit < 1 || limit > 10) {
-    return res
-      .status(400)
-      .json({ message: 'Invalid limit, limit must be 1 < limit < 10' });
-  }
-  if (skip < 0) {
-    return res.status(400).json({ message: 'Invalid skip, skip must be > 0' });
-  }
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
 
-  const userId = req.jwt!.id;
-  const user = await User.findById(userId).exec();
-  const userCount = await getUserCount(userId, res);
+    // Get the top 10 users with the highest count
+    const counts = await fetchLeaderboard(limit, skip);
 
-  if (!user) {
-    return res.status(404).json({ error: 'User not found' });
-  }
+    // Get the rank of the user in the database
+    const rank = await fetchRank(userId);
 
-  // Get the top 10 users with the highest count
-  const counts = await fetchLeaderboard(limit, skip);
+    const response = {
+      user: formatUser(user, userCount, req.jwt!),
+      rank: user.username && rank ? rank : null,
+      topUsers: [] as ReturnType<typeof formatUser>[],
+    };
+    for (const count of counts) {
+      response.topUsers.push(formatUser(count.user, count));
+    }
 
-  // Get the rank of the user in the database
-  const rank = await fetchRank(userId);
-
-  const response = {
-    user: formatUser(user, userCount, req.jwt!),
-    rank: user.username && rank ? rank : null,
-    topUsers: [] as ReturnType<typeof formatUser>[],
-  };
-  for (const count of counts) {
-    response.topUsers.push(formatUser(count.user, count));
-  }
-
-  res.json(response);
-});
+    res.json(response);
+  },
+);
 
 export default router;
