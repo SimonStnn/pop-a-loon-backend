2607514aba4a0fc7e692afdf64a5b491cd7cf47b
SimonStnn <simon.stijnen.23@gmail.com>
2025-02-08 15:10:23 +0100

    Add parameters to leaderboard endpoint #92

--- api/routes/leaderboard.ts
+++ api/routes/leaderboard.ts
@@ -10,14 +10,34 @@ router.get(
   '/',
   query('limit').isInt({ min: 1, max: 100 }).optional(),
   query('skip').isInt({ min: 0 }).optional(),
+  query('start-date')
+    .isISO8601()
+    .custom((value, { req }) => {
+      if (!req.query) return false;
+      if (new Date(value) > new Date(req.query['end-date']))
+        throw new Error('Start date must be before end date');
+
+      if (new Date(value) < new Date(2024, 6, 1))
+        throw new Error('Start date must be after 2024-07-01');
+      return true;
+    })
+    .optional(),
+  query('end-date').optional().isISO8601(),
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: 'Invalid query parameters' });
     }
-    const data = matchedData(req) as { limit?: any; skip?: any };
+    const data = matchedData(req) as {
+      limit?: any;
+      skip?: any;
+      'start-date'?: string;
+      'end-date'?: string;
+    };
     const limit = Number(data.limit) || 10;
     const skip = Number(data.skip) || 0;
+    const startDate = data['start-date'] ? new Date(data['start-date']) : null;
+    const endDate = data['end-date'] ? new Date(data['end-date']) : null;
     const userId = req.jwt!.id;
     const user = await User.findById(userId).exec();
 
@@ -28,7 +48,7 @@ router.get(
     const [userCount, counts] = await Promise.all([
       getUserCount(userId),
       // Get the top 10 users with the highest count
-      fetchLeaderboard(limit, skip, userId),
+      fetchLeaderboard(limit, skip, userId, startDate, endDate),
     ]);
     return res.json({
       user: formatUser(user, userCount, req.jwt!),
